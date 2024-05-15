e8968ea0a48645ee5b1579370e2e551ceeb7ab41
Mehmet Hanifi Kirazoğlu <103641323+hanifikiraz@users.noreply.github.com>
2024-05-15 15:25:33 +0300

    update

--- Assets/Scripts/PairingLevelSystem.cs
+++ Assets/Scripts/PairingLevelSystem.cs
@@ -7,6 +7,11 @@ using UnityEngine;
 public class PairingLevelSystem : MonoBehaviour
 {
 
+    [SerializeField] private List<GameObject> buttons;
+    [SerializeField] private GameObject particlesForWin;
+    [SerializeField] private GameObject buttonsBackGround;
+    [SerializeField] private GameObject congText;
+    [SerializeField] private List<ParticleSystem> partic;
     [SerializeField] private List<PairingButtons> pairingButtons;
 
     private void OnEnable()
@@ -28,16 +33,48 @@ public class PairingLevelSystem : MonoBehaviour
             if (pairingButtons[0].GetComponent<PairingButtons>().objectID == pairingButtons[1].GetComponent<PairingButtons>().objectID )
             {
                 Debug.Log("TRUE");
+                BusSystem.CallPlayerSetAnim(2);
                 foreach (var VARIABLE in pairingButtons)
                 {
-                    VARIABLE.transform.DOScale(Vector3.zero, 0.3f);
+                    VARIABLE.transform.DOScale(Vector3.zero, 0.3f).OnComplete(LevelEndController);
                 }
             }
             else
             {
                 Debug.Log("FALSE");
+                BusSystem.CallPlayerSetAnim(3);
             }
             pairingButtons.Clear();
         }   
+    }
+
+    private void LevelEndController()
+    {
+        int value = 0;
+        foreach (var VARIABLE in buttons)
+        {
+            if (VARIABLE.transform.localScale.z == 0)
+            {
+                value++;
+            }
+        }
+
+        if (value == buttons.Count)
+        {
+            Debug.Log("LEVEL ENDD  " + value);
+            StartCoroutine(LevelEndAnim());
+        }
+    }
+    private IEnumerator LevelEndAnim()
+    {
+        buttonsBackGround.transform.DOMoveY(-5, 1f);
+        yield return new WaitForSecondsRealtime(0.4f);
+        particlesForWin.SetActive(true);
+        congText.SetActive(true);
+        congText.gameObject.transform.DOScale(Vector3.zero, 0.7f).From();
+        foreach (var VARIABLE in partic)
+        {
+            VARIABLE.Play();
+        }
     }
 }
