4e337632751fd74b7071a886c93ef19592d3b8bb
Mehmet Hanifi Kirazoğlu <103641323+hanifikiraz@users.noreply.github.com>
2024-06-06 13:04:36 +0300

    update

--- Assets/Scripts/Answers/ButtonSelection.cs
+++ Assets/Scripts/Answers/ButtonSelection.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using DG.Tweening;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Answers
 {
@@ -16,15 +19,40 @@ namespace Answers
         [SerializeField] private Answer answerType;
         [SerializeField] private string buttonTextAnswer;
         [SerializeField] private TextMeshProUGUI buttonText;
+        [SerializeField] private Image defaultImage;
+        [SerializeField] private List<Sprite> sprites;
         private Tween scaleTween;
         private bool onClickable = true;
         
         private void Start()
         {
             buttonText.text = buttonTextAnswer;
+            SetButtonImage(1);
             //TweenAnimationLittle();
         }
 
+        private void SetButtonImage(int value)
+        {
+            switch (value)
+            {
+                case 1:
+                    defaultImage.sprite = sprites[0];
+                    break;
+                case 2:
+                    StartCoroutine(SetWrongImage());
+                    break;
+                case 3:
+                    defaultImage.sprite = sprites[2];
+                    break;
+            }
+        }
+
+        private IEnumerator SetWrongImage()
+        {
+            defaultImage.sprite = sprites[1];
+            yield return new WaitForSecondsRealtime(0.5f);
+            defaultImage.sprite = sprites[0];
+        }
         private void OnEnable()
         {
             BusSystem.OnButtonClickable += ButtonClickable;
@@ -64,11 +92,13 @@ namespace Answers
                 switch (answerType)
                 {
                     case Answer.True:
+                        SetButtonImage(3);
                         Debug.Log("True");
                         BusSystem.CallPlayerSetAnim(3);
                         BusSystem.CallWrongAnswer(true);
                         break;
                     case Answer.False:
+                        SetButtonImage(2);
                         Debug.Log("False");
                         BusSystem.CallPlayerSetAnim(2);
                         BusSystem.CallWrongAnswer(false);
